d0d0eed9458cf9ffd4e01c6f8b9bebf04fea01e1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-16 07:09:46 +0000

    Deploy arnavchauhan.me to github.com/foxy17/PortfolioWebsite.git:gh-pages

---
+++ precache-manifest.32554c346074d9bdeaab1899dfcd2f18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470fde06deb7599bf4cd6aae55ab1fed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6820740eca2a3a0d01c9",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3a4301282a269506839b",
+    "url": "/static/css/main.4ba83f93.chunk.css"
+  },
+  {
+    "revision": "6820740eca2a3a0d01c9",
+    "url": "/static/js/2.e3630cfc.chunk.js"
+  },
+  {
+    "revision": "2f71fbc01fec0c62d6e32185bc021541",
+    "url": "/static/js/2.e3630cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4301282a269506839b",
+    "url": "/static/js/main.a0a165e5.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "f3c6d0fdc092ce49fa8e0a92d0f66ea0",
+    "url": "/static/media/arnav_CV.f3c6d0fd.pdf"
+  },
+  {
+    "revision": "d11e0b83b73b61624e83842a66f41b26",
+    "url": "/static/media/youngun.d11e0b83.JPG"
+  }
+]);
